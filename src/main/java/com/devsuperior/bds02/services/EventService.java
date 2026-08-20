72bc9121672bef5003289bb33d8ae1795d34d8f6
Allex <1allex.moura@gmail.com>
2026-08-19 21:13:42 -0300

    Event - repository, service, controller. Testes aprovados

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,38 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getReferenceById(id);
+            copyDtoToEntity(entity, dto);
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+    }
+
+    private void copyDtoToEntity(Event entity, EventDTO dto) {
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(new City(dto.getCityId(), null));
+    }
+
+}
